7cf72da32f3183a557126be3ae750ed75608d3ba
kavin <kavin@LAPTOP-1REN5OBU>
2021-08-08 23:34:51 -0400

    added model 2

--- src/main.py
+++ src/main.py
@@ -14,37 +14,12 @@ import numpy as np
 
 app = FastAPI()
 
-
-# In[2]:
-
-
 #best_model = wandb.restore(
 #  'model-best.h5', run_path="kavinv/imdb_sentiment_classification/runs/2xllp48u")
 
 
-# In[3]:
-
-
 #getting the model
 model = tf.keras.models.load_model('Model')
-
-
-# In[4]:
-
-
-#import nest_asyncio
-#nest_asyncio.apply()
-#__import__('IPython').embed()
-
-
-# In[6]:
-
-
-
-
-
-# In[ ]:
-
 
 class Reviews(BaseModel):
     review: str
@@ -78,10 +53,3 @@ def predict_review(data: Reviews):
 
 if __name__ == '__main__':
    uvicorn.run(app, host='127.0.0.1', port=8003)
-
-
-# In[ ]:
-
-
-
-
